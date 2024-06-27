4401cedc1905b6765eabf8ff53b7a609573d161f
kevinhaube <4022304+kevinhaube@users.noreply.github.com>
2024-06-27 15:22:00 -0400

    lazy-load images

--- components/AlbumGrid/AlbumGrid.tsx
+++ components/AlbumGrid/AlbumGrid.tsx
@@ -1,12 +1,13 @@
 import { TypedLegacyEntry } from "@/data/legacy/types";
+import Image from 'next/image'
 
 const AlbumGrid = ({ albums }: { albums: TypedLegacyEntry[] }) => {
     return (
         <div className="w-[96.5vw] grid grid-cols-2 md:grid-cols-4 gap-2">
             {albums?.length > 0 ? (
                 albums.map((album, index) => (
-                    <div key={index} className="col-span-1 aspect-square bg-gray-900 flex justify-center items-center">
-                        <img src={album.Artwork} alt={album.Title} className="object-cover w-full" />
+                    <div key={index} className="col-span-1 aspect-square bg-gray-200 flex justify-center items-center">
+                        <Image src={album.Artwork} alt={album.Title} width={300} height={300} className="object-cover w-full" />
                     </div>
                 ))
             ) : <em className={"text-gray-500"}>No data</em>}
--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'dwnodxgkqevbqfkfyggd.supabase.co',
+        port: '',
+        pathname: '/storage/v1/object/public/artwork/**',
+      },
+    ],
+  },
   async redirects() {
     return [
       {
