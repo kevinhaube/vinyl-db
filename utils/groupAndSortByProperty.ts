a5c36a5af3a8185c6ed67a91504e08ba84e27178
kevinhaube <4022304+kevinhaube@users.noreply.github.com>
2024-06-26 20:02:58 -0400

    update underlying groupAndSortByProperty func

--- utils/groupAndSortByProperty.ts
+++ utils/groupAndSortByProperty.ts
@@ -1,6 +1,6 @@
 import exp from 'node:constants';
 
-export function groupAndSortByProperty<T>(arr: T[], groupBy: keyof T, filterGroupBy?: keyof T): T[][] {
+export function groupAndSortByProperty<T>(arr: T[], groupBy: keyof T, sortGroupBy?: keyof T): T[][] {
   // Step 1: Group by the specified property
   const grouped = arr.reduce((acc: { [key: string]: T[] }, obj: T) => {
     const key = String(obj[groupBy]);
@@ -12,8 +12,8 @@ export function groupAndSortByProperty<T>(arr: T[], groupBy: keyof T, filterGrou
   }, {});
   // Step 2: Sort each group alphabetically by the property
   for (let key in grouped) {
-    const filterProp = !filterGroupBy ? groupBy : filterGroupBy
-    grouped[key].sort((a, b) => String(a[filterProp]).localeCompare(String(b[filterProp])));
+    const sortProp = !sortGroupBy ? groupBy : sortGroupBy
+    grouped[key].sort((a, b) => String(a[sortProp]).localeCompare(String(b[sortProp])));
   }
   // Step 3: Convert the grouped object into a 2D array and sort the outer array alphabetically by the property
   return Object.keys(grouped)
