ac7345f30206a8f6ac7d29c77582b5e7e5579b12
kevinhaube <4022304+kevinhaube@users.noreply.github.com>
2024-06-27 22:44:06 -0400

    updte redirect

--- next.config.mjs
+++ next.config.mjs
@@ -14,7 +14,7 @@ const nextConfig = {
     return [
       {
         source: '/',
-        destination: '/browse/recently-added',
+        destination: '/browse/newest',
         permanent: true,
       },
     ]
